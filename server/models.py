22987f7c8e84290b8acf28fea86957484f6e9780
Katya Weicht <katya.weicht@gmail.com>
2025-07-31 16:59:29 -0600

    Set up initial models file

---
+++ server/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from marshmallow import Schema, fields
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class User(db.Model):
+  pass
+
+class Mood(db.Model):
+  pass
