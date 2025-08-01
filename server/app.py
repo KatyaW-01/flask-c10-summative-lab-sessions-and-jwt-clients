77946ca3acc789961cf6016ed805dbdd00cecf5d
Katya Weicht <katya.weicht@gmail.com>
2025-08-01 11:45:14 -0600

    Create skeleton of endpoints

--- server/app.py
+++ server/app.py
@@ -3,3 +3,46 @@ from flask import request, session
 from flask_restful import Resource
 from config import app, db, api
 from models import *
+
+class Signup(Resource):
+  def post(self):
+    pass
+
+class Login(Resource):
+  def post(self):
+    pass
+
+class CheckSession(Resource):
+  def get(self):
+    pass
+
+class Logout(Resource):
+  def delete(self):
+    pass
+
+class UserIndex(Resource):
+  def get(self):
+    #get all users and associated moods
+    #paginated
+    pass
+
+  def post(self):
+    #create a mood
+    pass
+
+class Moods(Resource):
+
+  def patch(self):
+    #update a mood
+    pass
+
+  def delete(self):
+    #delete a mood
+    pass
+
+api.add_resource(Signup, '/signup')
+api.add_resource(Login, '/login')
+api.add_resource(CheckSession, '/checksession')
+api.add_resource(Logout, '/logout')
+api.add_resource(UserIndex, '/users')
+api.add_resource(Moods, '/moods/<id>')
