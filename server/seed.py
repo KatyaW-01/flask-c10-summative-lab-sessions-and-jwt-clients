232ea67f0db0a74060fbb020b68301c2891d7077
Katya Weicht <katya.weicht@gmail.com>
2025-07-31 18:01:36 -0600

    Add users to the seed file

--- server/seed.py
+++ server/seed.py
@@ -2,5 +2,21 @@
 
 from app import app
 from models import *
+
 with app.app_context():
-  pass
+  
+  print("Deleting all records...")
+
+  User.query.delete()
+  MoodTracker.query.delete()
+
+  print("Creating Users...")
+  user1 = User(name="Josh")
+  user2 = User(name="Mary")
+  user3 = User(name="Tom")
+  user4 = User(name="Mark")
+  user5 = User(name="Betty")
+
+  db.session.add_all([user1,user2,user3,user4,user5])
+
+  print("Creating User's moods...")
